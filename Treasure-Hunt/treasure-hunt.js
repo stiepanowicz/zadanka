eff61b5ffac8a8841614f8a317fe2c4da05688dd
stiepanowicz <stiepanbogdan@gmail.com>
2025-03-02 20:17:21 +0100

    treasure-hunt solved

--- Treasure-Hunt/treasure-hunt.js
+++ Treasure-Hunt/treasure-hunt.js
@@ -1,3 +1,5 @@
+const { useAnimationControls } = require("framer-motion");
+
 treasureMap = [
     [34,21,32,41,25],
     [14,42,43,14,31],
@@ -5,15 +7,29 @@ treasureMap = [
     [33,15,51,31,35],
     [21,52,33,13,23]
 ];
+
 let currentLocation = treasureMap[0][0];
-console.log(currentLocation);
-// currentLocation.
+let newLocation = [];
+
 function readDestination(currentLocation) {
-    currentLocation;
+    let coordinates = currentLocation.toString().split("");
     return coordinates;
 }
 
-function nextStop(destination) {
-    return currentLocation;
+function nextStop(coordinates) {
+    let x = coordinates[0] -1;
+    let y = coordinates[1] -1;
+    console.log(`Current coordinates are: ${x+1}${y+1}`);
+    return treasureMap[x][y];
 }
 
+while (true) {
+
+    newLocation = nextStop(readDestination(currentLocation));
+    if (currentLocation === newLocation) {
+        break;
+    };
+    currentLocation = newLocation;
+}
+
+console.log(`The treasure is at ${newLocation}`);
